4d56ed213558ded3196674778ea4fb08b05ec31e
ConnorDoane <2computers73@gmail.com>
2017-05-04 11:48:52 -0600

    basic home page

---
+++ homeScript.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	console.log('jQuery Ready!');
+	$('#titleButton').click(function() {
+		window.location = 'home.html';
+	})
+	$('#classesButton').click(function() {
+		alert('Not Yet Implemented');
+	})
+	$('#optionsButton').click(function() {
+		alert('Not Yet Implemented');
+	})
+	$('#logOutButton').click(function() {
+		alert('Not Yet Implemented');
+	})
+})
